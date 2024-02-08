cfc3d926750e22df5ba9d01fe9355976cc4b7f14
Pristine <prisvsyo@gmail.com>
2024-02-08 13:26:47 -0800

    completed problem 7

--- problems/07-value-pair.js
+++ problems/07-value-pair.js
@@ -11,8 +11,20 @@ valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 ***********************************************************************/
 
 function valuePair(obj1, obj2, key) {
-  // Your code here 
+  // Your code here
+  let result = [];
+  result.push(obj1[key]);
+  result.push(obj2[key]);
+
+  return result;
 }
+
+//Examples:
+
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+console.log(valuePair(object1, object2, 'location')); // => [ 'NY', 'SF' ]
+console.log(valuePair(object1, object2, 'name')); // => [ 'One', 'Two' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuePair;
