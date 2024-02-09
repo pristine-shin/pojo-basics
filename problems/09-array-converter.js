17b53d7b145cbe22991dcf3cc904fe25060f8602
Pristine <prisvsyo@gmail.com>
2024-02-08 17:58:31 -0800

    struggle bus problem 9

--- problems/09-array-converter.js
+++ problems/09-array-converter.js
@@ -11,9 +11,68 @@ console.log(arrayConverter(["mango", "pineapple"])); // => {mango: 1, pineapple:
 console.log(arrayConverter(["apple", "banana", "potato", "banana"])); // => {apple: 1, banana: 2, potato: 1}
 ***********************************************************************/
 
+//First draft lmao. Nested loop runs into issues counting, do it the next way.
+// function arrayConverter(array) {
+//   // Your code here
+//   let count = 0;
+//   let obj = {};
+
+//   for (let i = 0; i < array.length; i++) {
+//     let item1 = array[i];
+
+//     for (let j = i + 1; j < array.length; j++) {
+//       let item2 = array[j];
+
+//       // console.log(item1, item2)
+//       if (item1 === item2) {
+//         count += 1
+//       }
+//     }
+//     obj[item1] = 1 + count;
+//   }
+//   return obj;
+
+// }
+//----------------------------------------------------------------------
 function arrayConverter(array) {
-  // Your code here 
+  let obj = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i];
+
+ // Check if item is already a property in the obj, increase
+    if (!obj[item]) {
+      obj[item] = 1;
+    } else {
+      // If not, initialize it with count 1
+      obj[item]++;
+    }
+  }
+
+  return obj;
 }
+//Savonna's work:
+// function arrayConverter(array) {
+//   //count is in an object
+//   let counter = {};
+//   //iterate through the array
+//   //if not in the object, add into the array
+//   //if it is in the object, increment
+//   for (let value of array) {
+//     if (counter[value] === undefined) {
+//       counter[value] = 1;
+//     } else {
+//       counter[value]++;
+//     }
+//   }
+
+//   return counter;
+// }
+//Examples:
+
+console.log(arrayConverter(["apple", "apple"])); // => {apple: 2}
+console.log(arrayConverter(["mango", "pineapple"])); // => {mango: 1, pineapple: 1}
+console.log(arrayConverter(["banana", "banana", "apple", "potato"])); // => {apple: 1, banana: 2, potato: 1}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = arrayConverter;
--- problems/10-string-converter.js
+++ problems/10-string-converter.js
@@ -12,8 +12,13 @@ console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 ***********************************************************************/
 
 function stringConverter(string) {
-  // Your code here 
+  // Your code here
+  
 }
 
+//Examples:
+console.log(stringConverter("apple")); // => {a: 1, p: 2, l: 1, e: 1}
+console.log(stringConverter("banana")); // => {b: 1, a: 3, n: 2}
+console.log(stringConverter("raccoon")); // => {r: 1, a: 1, c: 2, o: 2, n: 1}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = stringConverter;
